e9b8cf862c6645caf42c516f6285692b63c286ea
YanarAssaf <yanar@outlook.com>
2020-05-24 17:37:56 +0300

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -19,7 +19,7 @@
   </div>
   <div class="card">
     <div class="card-body login-card-body">
-      <p class="login-box-msg">Sign in to start your session :D</p>
+      <p class="login-box-msg">Sign in to start your session </p>
 
       <form method="POST" action="{{ route('login') }}">
         @csrf
